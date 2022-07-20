2be970eb08a6d04162bd6be8bb0794b624025db2
王文文 <wangww0728@thundersoft.com>
2022-07-20 17:27:36 +0800

    修改包结构

---
+++ eladmin-ha/src/main/java/site/hyxy/EladminHaApplication.java
@@ -0,0 +1,13 @@
+package site.hyxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EladminHaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EladminHaApplication.class, args);
+    }
+
+}
---
+++ eladmin-ha/src/test/java/site/hyxy/EladminHaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.hyxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminHaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
